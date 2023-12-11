6b2386d05cbe1d2882eaaf548e0fffd69be3a57e
shanu <shanu@192.168.0.15>
2023-12-10 23:35:15 -0500

    CHANGED STATES

---
+++ src/myapp/welcome.java
@@ -0,0 +1,12 @@
+package myapp;
+
+public class welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.printIn("welcome to the world");
+
+	}
+
+}
